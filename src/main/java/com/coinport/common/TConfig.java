70561693787f8599ef34e809586336fc40294b29
mmmmmagina <jaice_229@163.com>
2014-09-25 16:32:51 +0800

    add dependencies and json config parser

---
+++ src/main/java/com/coinport/common/TConfig.java
@@ -0,0 +1,12 @@
+
+package com.coinport.common;
+
+public class TConfig {
+    // common flag put here
+    public String serviceName;
+
+    public TConfig() {
+        // default value
+        serviceName = "config";
+    }
+}
---
+++ src/main/java/com/coinport/exchange/AbstractExchange.java
@@ -0,0 +1,24 @@
+package com.coinport.exchange;
+
+public class AbstractExchange {
+
+	private String accessKey;
+	private String secureKey;
+
+	public String getAccessKey() {
+		return accessKey;
+	}
+
+	public void setAccessKey(String accessKey) {
+		this.accessKey = accessKey;
+	}
+
+	public String getSecureKey() {
+		return secureKey;
+	}
+
+	public void setSecureKey(String secureKey) {
+		this.secureKey = secureKey;
+	}
+
+}
